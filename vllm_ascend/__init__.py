91bad8c29fe9f19896c4c142084fdd99159a9cb5
wu_yushan <wuyushan1@huawei.com>
2026-03-04 11:11:33 +0800

    add model register for test

--- vllm_ascend/__init__.py
+++ vllm_ascend/__init__.py
@@ -38,3 +38,7 @@ def register_service_profiling():
     from .profiling_config import generate_service_profiling_config
 
     generate_service_profiling_config()
+
+def register_model():
+    from .models import register_model
+    register_model()
