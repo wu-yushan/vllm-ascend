37b7439f87672cc84035218e2a93e3cd586af162
wu_yushan <wuyushan1@huawei.com>
2026-03-04 17:06:36 +0800

    fix init import

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -4,7 +4,7 @@ import vllm_ascend.envs as envs
 
 
 def register_model():
-    from .llama_eagle3_vwn import CustomQwen3ForCausalLM  # noqa: F401
+    from .llama_eagle3_vwn import Eagle3VwnLlamaForCausalLM  # noqa: F401
 
     ModelRegistry.register_model(
         "LlamaForCausalLMVwnEagle3", "vllm_ascend.models.llama_eagle3_vwn:Eagle3VwnLlamaForCausalLM")
