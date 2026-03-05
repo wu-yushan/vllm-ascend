d6195d98bbf42bfa322a517570c23acee9e75fb5
wu_yushan <wuyushan1@huawei.com>
2026-03-05 16:31:28 +0800

    first version of vwm_ealge3

--- vllm_ascend/models/llama_eagle3_vwn.py
+++ vllm_ascend/models/llama_eagle3_vwn.py
@@ -11,31 +11,153 @@ from vllm.compilation.decorators import support_torch_compile
 from vllm.config import VllmConfig, get_current_vllm_config
 from vllm.logger import init_logger
 from vllm.model_executor.layers.layernorm import RMSNorm
-from vllm.model_executor.layers.linear import QKVParallelLinear, ReplicatedLinear
-from vllm.model_executor.layers.logits_processor import LogitsProcessor
-from vllm.model_executor.layers.quantization.base_config import QuantizationConfig
-from vllm.model_executor.layers.vocab_parallel_embedding import (
-    ParallelLMHead,
-    VocabParallelEmbedding,
-)
+from vllm.model_executor.layers.linear import ReplicatedLinear
 from vllm.model_executor.model_loader.weight_utils import (
     default_weight_loader,
     maybe_remap_kv_scale_name,
 )
-from vllm.model_executor.models.llama import LlamaDecoderLayer, LlamaForCausalLM
-from vllm.multimodal.inputs import NestedTensors
+from vllm.model_executor.models.llama_eagle3 import Eagle3LlamaForCausalLM, LlamaDecoderLayer
 
 from vllm.model_executor.models.utils import (
     AutoWeightsLoader,
-    get_draft_quant_config,
     maybe_prefix,
     process_eagle_weight,
 )
 
 logger = init_logger(__name__)
 
+class PreVwnLayerV0(nn.Module):
+    def __init__(
+        self,
+        vllm_config: VllmConfig,
+        prefix: str = "",
+        config: LlamaConfig | None = None
+    ) -> None:
+        super().__init__()
+        config = config or vllm_config.model_config.hf_config
+        quant_config = self.get_quant_config(vllm_config)
+        hidden_size = config.hidden_size
+        m = getattr(config, "m_size", 1)
+        expanded_factor = getattr(config, "expanded_factor", 1)
+        wider_dim = hidden_size * expanded_factor
 
-class LlamaDecoderLayer(LlamaDecoderLayer):
+        self.wider_dim = wider_dim
+        
+        self.upward = ReplicatedLinear(
+            input_size=hidden_size,
+            output_size=wider_dim,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "upward"),
+            return_bias=False,
+        )
+        extend_size = 2 * wider_dim
+        self.extend = ReplicatedLinear(
+            input_size=wider_dim,
+            output_size=extend_size,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "extend"),
+            return_bias=False,
+        )
+        self.input_layernorm = RMSNorm(wider_dim, eps=config.rms_norm_eps)
+        self.hidden_norm = RMSNorm(wider_dim, eps=config.rms_norm_eps)
+        self.fc = ReplicatedLinear(
+            input_size=extend_size,
+            output_size=wider_dim,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "fc"),
+            return_bias=False,
+        )
+        self.full_connected = ReplicatedLinear(
+            input_size=wider_dim,
+            output_size=wider_dim,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "extend"),
+            return_bias=False,
+        )
+
+    def forward(
+        self,
+        embeds: torch.Tensor,
+        hidden_states: torch.Tensor
+    ):
+        wider_embeds = self.upward(embeds)
+        wider_hidden_states = self.upward(hidden_states)
+
+        extended_hidden_states = self.extend(wider_hidden_states)
+        hidden_states_to_cat, hidden_residual = torch.split(
+            extended_hidden_states,
+            [self.wider_dim, extended_hidden_states.shape[-1] - self.wider_dim],
+            dim=-1
+        )
+        norm_wider_embeds = self.input_layernorm(wider_embeds)
+        hidden_states_to_cat = self.hidden_norm(hidden_states_to_cat)
+        hidden_to_fc = torch.cat([norm_wider_embeds, hidden_states_to_cat], dim=-1)
+        hidden_after_fc = self.fc(hidden_to_fc)
+        hidden_before_residual = self.full_connected(hidden_after_fc)
+        wider_hidden_states = hidden_before_residual + hidden_residual
+
+        return wider_hidden_states
+
+class PreVwnLayerV1(nn.Module):
+    def __init__(
+        self,
+        vllm_config: VllmConfig,
+        prefix: str = "",
+        config: LlamaConfig | None = None
+    ) -> None:
+        super().__init__()
+        config = config or vllm_config.model_config.hf_config
+        quant_config = self.get_quant_config(vllm_config)
+        hidden_size = config.hidden_size
+        m = getattr(config, "m_size", 1)
+        expanded_factor = getattr(config, "expanded_factor", 1)
+        wider_dim = hidden_size * expanded_factor
+        self.wider_dim = wider_dim
+
+        self.input_layernorm = RMSNorm(hidden_size, eps=config.rms_norm_eps)
+        self.hidden_norm = RMSNorm(hidden_size, eps=config.rms_norm_eps)
+        fc_input_size = 2 * hidden_size
+        self.fc = ReplicatedLinear(
+            input_size=fc_input_size,
+            output_size=hidden_size,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "fc"),
+            return_bias=False,
+        )
+        self.upward = ReplicatedLinear(
+            input_size=hidden_size,
+            output_size=wider_dim,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "upward"),
+            return_bias=False,
+        )
+
+    def forward(
+        self,
+        embeds: torch.Tensor,
+        hidden_states: torch.Tensor
+    ):
+        norm_embeds = self.input_layernorm(embeds)
+        norm_hidden = self.hidden_norm(hidden_states)
+        hidden_to_fc = torch.cat([norm_embeds, norm_hidden], dim=-1)
+        hidden_after_fc = self.fc(hidden_to_fc)
+        wider_hidden_states = self.upward(hidden_after_fc)
+
+        return wider_hidden_states
+
+class VwnLlamaDecoderLayer(LlamaDecoderLayer):
     def __init__(
         self,
         vllm_config: VllmConfig,
@@ -47,47 +169,77 @@ class LlamaDecoderLayer(LlamaDecoderLayer):
 
         config = config or vllm_config.model_config.hf_config
         quant_config = self.get_quant_config(vllm_config)
+        m = getattr(config, "m_size", 1)
+        expanded_factor = getattr(config, "expanded_factor", 1)
+        self.hidden_size = config.hiddensize
+        wider_dim = self.hidden_size * expanded_factor
+        self.wider_dim = wider_dim
 
-        # First layer uses 2*hidden_size (embeds + hidden_states concatenated)
-        # Subsequent layers use hidden_size (only hidden_states, no embeds)
-        qkv_input_size = 2 * self.hidden_size if layer_idx == 0 else self.hidden_size
+        if getattr(config, "eable_pre_vmn", True):
+            self.pre_vwn_layer = PreVwnLayerV0(
+                vllm_config,
+                prefix=maybe_prefix(prefix, f"layers.pre_vwn_layer"),
+                config=self.config,
+                layer_idx=layer_idx,
+            )
+        else:
+            self.pre_vwn_layer = PreVwnLayerV1(
+                vllm_config,
+                prefix=maybe_prefix(prefix, f"layers.pre_vwn_layer"),
+                config=self.config,
+                layer_idx=layer_idx,
+            )
 
-        # override qkv
-        self.self_attn.qkv_proj = QKVParallelLinear(
-            qkv_input_size,
-            self.self_attn.head_dim,
-            self.self_attn.total_num_heads,
-            self.self_attn.total_num_kv_heads,
+        self.downward_and_forgot = ReplicatedLinear(
+            input_size=wider_dim,
+            output_size=self.hidden_size + wider_dim,
             bias=False,
+            params_dtype=vllm_config.model_config.dtype,
             quant_config=quant_config,
-            prefix=maybe_prefix(prefix, "qkv_proj"),
+            prefix=maybe_prefix(prefix, "downward_and_forgot"),
+            return_bias=False,
+        )
+        
+        self.pre_attention_layernorm = RMSNorm(self.hidden_size, eps=config.rms_norm_eps)
+        self.upward_after_attn = ReplicatedLinear(
+            input_size=self.hidden_size,
+            output_size=wider_dim,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "upward_after_attn"),
+            return_bias=False,
+        )
+        self.downward_and_forgot_after_attn = ReplicatedLinear(
+            input_size=wider_dim,
+            output_size=self.hidden_size + wider_dim,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "downward_and_forgot"),
+            return_bias=False,
+        )
+        self.post_attention_layernorm = RMSNorm(self.hidden_size, eps=config.rms_norm_eps)
+        self.upward_after_mlp = ReplicatedLinear(
+            input_size=self.hidden_size,
+            output_size=wider_dim,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "upward_after_mlp"),
+            return_bias=False,
+        )
+        self.downward = ReplicatedLinear(
+            input_size=wider_dim,
+            output_size=self.hidden_size,
+            bias=False,
+            params_dtype=vllm_config.model_config.dtype,
+            quant_config=quant_config,
+            prefix=maybe_prefix(prefix, "downward"),
+            return_bias=False,
         )
 
-        self.hidden_norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.layer_idx = layer_idx
-
-        if getattr(config, "norm_before_residual", False):
-            self._residual_norm = self._norm_before_residual
-        else:
-            self._residual_norm = self._norm_after_residual
-
-    def get_quant_config(self, vllm_config: VllmConfig) -> QuantizationConfig | None:
-        """Use drafter's quantization config instead of verifier's."""
-        return get_draft_quant_config(vllm_config)
-
-    def _norm_before_residual(
-        self, hidden_states: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        hidden_states = self.hidden_norm(hidden_states)
-        residual = hidden_states
-        return hidden_states, residual
-
-    def _norm_after_residual(
-        self, hidden_states: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        residual = hidden_states
-        hidden_states = self.hidden_norm(hidden_states)
-        return hidden_states, residual
 
     def forward(
         self,
@@ -97,24 +249,38 @@ class LlamaDecoderLayer(LlamaDecoderLayer):
         residual: torch.Tensor | None,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         if self.layer_idx == 0:
-            # First layer: concatenate embeds with hidden_states
-            embeds = self.input_layernorm(embeds)
-            hidden_states, residual = self._residual_norm(hidden_states=hidden_states)
-            hidden_states = torch.cat([embeds, hidden_states], dim=-1)
-        else:
-            # Subsequent layers: process hidden_states and residuals only
-            hidden_states, residual = self.input_layernorm(hidden_states, residual)
+            # First layer: preVwn + vwn layer
+            wider_hidden_states = self.pre_vwn_layer(embeds, hidden_states)
 
-        # Self Attention
-        hidden_states = self.self_attn(
-            positions=positions,
-            hidden_states=hidden_states,
-        )
+            # attn
+            total_hidden_states = self.downward_and_forgot(wider_hidden_states)
+            hidden_states, hidden_residual = torch.split(
+                total_hidden_states,
+                [self.hidden_size, total_hidden_states.shape[-1] - self.hidden_size],
+                dim=-1
+            )
+            hidden_states = self.pre_attention_layernorm(hidden_states)
+            hidden_states = self.self_attn(
+                positions=positions,
+                hidden_states=hidden_states,
+            )
+            upward_hidden_states = self.upward_after_attn(hidden_states)
+            wider_hidden_states = upward_hidden_states + hidden_residual
 
-        hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
+            # mlp
+            total_hidden_states = self.downward_and_forgot_after_attn(wider_hidden_states)
+            hidden_states, hidden_residual = torch.split(
+                total_hidden_states,
+                [self.hidden_size, total_hidden_states.shape[-1] - self.hidden_size],
+                dim=-1
+            )
+            hidden_states = self.post_attention_layernorm(hidden_states)
+            hidden_states = self.mlp(hidden_states)
+            upward_hidden_states = self.upward_after_mlp(hidden_states)
+            wider_hidden_states = upward_hidden_states + hidden_residual
 
-        # Fully Connected
-        hidden_states = self.mlp(hidden_states)
+            # downward
+            hidden_states = self.downward(wider_hidden_states)
 
         return hidden_states, residual
 
@@ -136,29 +302,11 @@ class LlamaModel(nn.Module):
         prefix: str = "",
     ) -> None:
         super().__init__()
-        self.config = vllm_config.speculative_config.draft_model_config.hf_config
-        self.vocab_size = self.config.vocab_size
-
-        # Get drafter's quantization config
-        self.quant_config = get_draft_quant_config(vllm_config)
-
-        eagle_config = getattr(self.config, "eagle_config", None)
-        if eagle_config is not None and "use_aux_hidden_state" in eagle_config:
-            self.use_aux_hidden_state = eagle_config["use_aux_hidden_state"]
-        else:
-            self.use_aux_hidden_state = True
-
         current_vllm_config = get_current_vllm_config()
-
-        self.embed_tokens = VocabParallelEmbedding(
-            self.config.vocab_size,
-            self.config.hidden_size,
-            prefix=maybe_prefix(prefix, "embed_tokens"),
-        )
-
+        # overwrite layers
         self.layers = nn.ModuleList(
             [
-                LlamaDecoderLayer(
+                VwnLlamaDecoderLayer(
                     current_vllm_config,
                     prefix=maybe_prefix(prefix, f"layers.{layer_idx + start_layer_id}"),
                     config=self.config,
@@ -167,27 +315,6 @@ class LlamaModel(nn.Module):
                 for layer_idx in range(self.config.num_hidden_layers)
             ]
         )
-        if self.use_aux_hidden_state:
-            if hasattr(self.config, "target_hidden_size"):
-                fc_input_size = self.config.target_hidden_size * 3
-            else:
-                fc_input_size = self.config.hidden_size * 3
-            self.fc = ReplicatedLinear(
-                input_size=fc_input_size,
-                output_size=self.config.hidden_size,
-                bias=False,
-                params_dtype=vllm_config.model_config.dtype,
-                quant_config=self.quant_config,
-                prefix=maybe_prefix(prefix, "fc"),
-                return_bias=False,
-            )
-        self.norm = RMSNorm(
-            self.config.hidden_size,
-            eps=self.config.rms_norm_eps,
-        )
-
-    def embed_input_ids(self, input_ids: torch.Tensor) -> torch.Tensor:
-        return self.embed_tokens(input_ids)
 
     def forward(
         self,
@@ -259,89 +386,9 @@ class LlamaModel(nn.Module):
         return loaded_params
 
 
-class Eagle3VwnLlamaForCausalLM(LlamaForCausalLM):
+class Eagle3VwnLlamaForCausalLM(Eagle3LlamaForCausalLM):
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         nn.Module.__init__(self)
-        self.config = vllm_config.speculative_config.draft_model_config.hf_config
-        # Ensure draft_vocab_size is set
-        # default to the base vocab size when absent
-        if getattr(self.config, "draft_vocab_size", None) is None:
-            base_vocab_size = getattr(self.config, "vocab_size", None)
-            self.config.draft_vocab_size = base_vocab_size
-        target_layer_num = vllm_config.model_config.get_num_layers(
-            vllm_config.parallel_config
-        )
-
-        # Store target layer count in draft config for
-        # proper layer_types indexing in draft models
-        self.config.target_layer_count = target_layer_num
-        self.model = LlamaModel(
-            vllm_config=vllm_config, prefix="model", start_layer_id=target_layer_num
-        )
-
-        logit_scale = getattr(self.config, "logit_scale", 1.0)
-        self.lm_head = ParallelLMHead(
-            self.config.draft_vocab_size,
-            self.config.hidden_size,
-            prefix=maybe_prefix(prefix, "lm_head"),
-        )
-        self.logits_processor = LogitsProcessor(
-            self.config.draft_vocab_size, scale=logit_scale
-        )
-        self.draft_id_to_target_id = nn.Parameter(
-            torch.zeros(self.config.draft_vocab_size, dtype=torch.long),
-            requires_grad=False,
-        )
-
-    def embed_input_ids(
-        self,
-        input_ids: torch.Tensor,
-        multimodal_embeddings: NestedTensors | None = None,
-        is_multimodal: torch.Tensor | None = None,
-    ) -> torch.Tensor:
-        return self.model.embed_input_ids(input_ids)
-
-    def forward(
-        self,
-        input_ids: torch.Tensor,
-        positions: torch.Tensor,
-        hidden_states: torch.Tensor,
-        inputs_embeds: torch.Tensor | None = None,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        return self.model(input_ids, positions, hidden_states, inputs_embeds)
-
-    def compute_logits(
-        self,
-        hidden_states: torch.Tensor,
-    ) -> torch.Tensor | None:
-        logits = self.logits_processor(self.lm_head, hidden_states)
-        if self.draft_id_to_target_id is None:
-            assert logits.shape[1] == self.config.vocab_size, (
-                "Expected logits to have shape "
-                f"(*, {self.config.vocab_size}), but got {logits.shape}"
-            )
-            return logits
-
-        base = torch.arange(self.config.draft_vocab_size, device=logits.device)
-        targets = base + self.draft_id_to_target_id
-        logits_new = logits.new_full(
-            (
-                logits.shape[0],
-                self.config.vocab_size,
-            ),
-            float("-inf"),
-        )
-        logits_new[:, targets] = logits
-        return logits_new
-
-    def combine_hidden_states(
-        self,
-        hidden_states: torch.Tensor,
-    ) -> torch.Tensor:
-        if not self.model.use_aux_hidden_state:
-            return hidden_states
-        # combine multiple auxiliary hidden states returned by eagle3
-        return self.model.fc(hidden_states)
 
     def load_weights(self, weights: Iterable[tuple[str, torch.Tensor]]):
         model_weights = {}
